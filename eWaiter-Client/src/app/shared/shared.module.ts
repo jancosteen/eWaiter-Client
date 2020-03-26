9fb27f6747c948cfabc755bb07f4264bf4f532e5
JancoTuks <u15033377@tuks.co.za>
2020-03-26 13:12:10 +0200

    Dialog components created

--- eWaiter-Client/src/app/shared/shared.module.ts
+++ eWaiter-Client/src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { SuccessDialogComponent } from './dialogs/success-dialog/success-dialog.component';
+import { ErrorDialogComponent } from './dialogs/error-dialog/error-dialog.component';
 
 //Remember to remove Material and Flex Modules from other modules and just import shared module
 
 @NgModule({
-  declarations: [],
+  declarations: [SuccessDialogComponent, ErrorDialogComponent],
   imports: [
     CommonModule,
     MaterialModule,
